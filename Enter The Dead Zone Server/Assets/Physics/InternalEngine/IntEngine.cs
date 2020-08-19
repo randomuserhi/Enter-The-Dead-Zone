2fa2a8a023753c134d6c2b634074ca91b21e0310
randomuserhi <magicalworld.nano@gmail.com>
2020-08-19 17:06:12 +0100

    Added InternalEngine
    InternalEngine is the physics engine I intend to use for my project, Currently no methods are implemented.
    
    In the future I intend the engine to work only with Circle and Box colliders using GPGPU programming for performance (This can be done using implementations such as ComputeShaders in Unity) => In this way unity will be used for rendering rather than as a physics engine
    
    Currently coding it seperately, will port to Server once written and tested

---
+++ Enter The Dead Zone Server/Assets/Physics/InternalEngine/IntEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalEngine
+{
+    public class IntEngine
+    {
+        public static void PerformTimeStep()
+        {
+
+        }
+    }
+}
