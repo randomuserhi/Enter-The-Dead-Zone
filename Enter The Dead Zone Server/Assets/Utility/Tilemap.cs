87f4bb15f10221876912bea28060087a18403129
randomuserhi <magicalworld.nano@gmail.com>
2020-11-08 23:09:03 +0000

    Added Tilemap template

--- Enter The Dead Zone Server/Assets/Utility/Tilemap.cs
+++ Enter The Dead Zone Server/Assets/Utility/Tilemap.cs
@@ -5,17 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 using DeadZoneEngine;
 using DeadZoneEngine.Entities;
 
-public class TilemapWrapper : AbstractWorldEntity
+public struct Tile
 {
-    private GameObject TilemapObject;
+}
 
-    public TilemapWrapper() { }
-    public TilemapWrapper(ulong ID) : base(ID) { }
+public class TilemapWrapper : PhysicalObject
+{
+    public Tile[] Map;
+    public Vector2Int Size;
+
+    public TilemapWrapper()
+    {
+        Init();
+    }
+    public TilemapWrapper(ulong ID) : base(ID)
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+    }
+
+    public override void Set(object Data)
+    {
+
+    }
 
     protected override void SetEntityType()
     {
@@ -27,3 +46,46 @@ public class TilemapWrapper : AbstractWorldEntity
         throw new NotImplementedException();
     }
 }
+
+
+/*public class TilemapWrapper : PhysicalObject
+{
+    private Tilemap Map;
+    private TilemapRenderer Renderer;
+    private TilemapCollider2D Collider;
+    private CompositeCollider2D CompositeCollider;
+
+    public TilemapWrapper()
+    {
+        Init();
+    }
+    public TilemapWrapper(ulong ID) : base(ID)
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        Map = Self.AddComponent<Tilemap>();
+        Renderer = Self.AddComponent<TilemapRenderer>();
+        Collider = Self.AddComponent<TilemapCollider2D>();
+        CompositeCollider = Self.AddComponent<CompositeCollider2D>();
+
+        Collider.usedByComposite = true;
+    }
+
+    public override void Set(object Data)
+    {
+        
+    }
+
+    protected override void SetEntityType()
+    {
+        Type = EntityType.Tilemap;
+    }
+
+    public override byte[] GetBytes()
+    {
+        throw new NotImplementedException();
+    }
+}*/
