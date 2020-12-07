7f6ae7f63ab27e1ed58b2890fdd3e0396ab27b90
randomuserhi <magicalworld.nano@gmail.com>
2020-12-07 13:16:43 +0000

    Fixed various tile height render

--- Enter The Dead Zone Server/Assets/Utility/Tilemap/Tilemap.cs
+++ Enter The Dead Zone Server/Assets/Utility/Tilemap/Tilemap.cs
@@ -83,7 +83,7 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
 
     public List<PhysicalObject> Sorting; 
 
-    private ComputeShader Compute;
+    private ComputeShader WallCompute;
     private ComputeShader FloorCompute;
     private int ComputeKernel;
 
@@ -100,7 +100,7 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
     //
     // This is to make rendering easier in the compute shader
 
-    ComputeBuffer MapBuffer;
+    ComputeBuffer WallBuffer;
     ComputeBuffer FloorBuffer;
 
     private RawImage FloorRender;
@@ -108,34 +108,38 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
     private RenderTexture Render;
     private RenderTexture FloorRenderTexture;
 
-    public TilePallet TilePalletData;
+    public TilePallet WallTilePalletData;
     public TilePallet FloorTilePalletData;
 
-    private int TileDimension;
+    private float TilesPerUnit = 1;
+    private int TileDimension = 32;
     private Vector2Int TilemapSize;
+    private Vector2 TilemapWorldSize;
 
-    public TilemapWrapper(int TileDimension, Vector2Int TilemapSize)
+    public TilemapWrapper(int TileDimension, Vector2Int TilemapSize, float TilesPerUnit = 1)
     {
         this.TileDimension = TileDimension;
         this.TilemapSize = TilemapSize;
+        TilemapWorldSize = (Vector2)TilemapSize / TilesPerUnit;
         Init();
     }
     public TilemapWrapper(ulong ID, int TileDimension, Vector2Int TilemapSize) : base(ID)
     {
         this.TileDimension = TileDimension;
         this.TilemapSize = TilemapSize;
+        TilemapWorldSize = (Vector2)TilemapSize / TilesPerUnit;
         Init();
     }
 
     //Destructor to clear buffers
     ~TilemapWrapper()
     {
-        if (MapBuffer != null)
+        if (WallBuffer != null)
         {
-            GC.SuppressFinalize(MapBuffer); //Dont actually know if this is safe, probably need to check, without it unity throws a warning
+            GC.SuppressFinalize(WallBuffer); //Dont actually know if this is safe, probably need to check, without it unity throws a warning
                                             //See https://forum.unity.com/threads/computebuffer-warning-when-component-uses-executeineditmode.844648/
                                             //    https://stackoverflow.com/questions/151051/when-should-i-use-gc-suppressfinalize
-            MapBuffer.Release();
+            WallBuffer.Release();
         }
     }
 
@@ -144,15 +148,15 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
         //Should change this into a more scalable system but for now this will do:
         AppliedRenderers.AddRange(GameObject.FindObjectsOfType<SpriteRenderer>());
 
-        if (Compute == null)
+        if (WallCompute == null)
         {
-            Compute = UnityEngine.Object.Instantiate(Resources.Load<ComputeShader>("ComputeShaders/TilemapComputeShader"));
-            ComputeKernel = Compute.FindKernel("TilemapRender");
+            WallCompute = UnityEngine.Object.Instantiate(Resources.Load<ComputeShader>("ComputeShaders/TilemapComputeShader"));
+            ComputeKernel = WallCompute.FindKernel("TilemapRender");
         }
         if (FloorCompute == null)
         {
             FloorCompute = UnityEngine.Object.Instantiate(Resources.Load<ComputeShader>("ComputeShaders/TilemapComputeShader"));
-            ComputeKernel = Compute.FindKernel("TilemapRender");
+            ComputeKernel = WallCompute.FindKernel("TilemapRender");
         }
 
         GenerateDefaultTileData();
@@ -166,20 +170,24 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
         Self.AddComponent<GraphicRaycaster>();
         Self.GetComponent<RectTransform>().sizeDelta = Vector2.zero;
         FloorRender = Self.AddComponent<RawImage>();
-        FloorRender.rectTransform.sizeDelta = new Vector2(TilemapSize.x, TilemapSize.y);
+        FloorRender.rectTransform.sizeDelta = new Vector2((float)TilemapSize.x / TilesPerUnit, (float)TilemapSize.y / TilesPerUnit);
         FloorRender.material = Resources.Load<Material>("Materials/LitMaterial");
         FloorRender.texture = FloorRenderTexture;
 
-        Render = new RenderTexture(TileDimension * TilemapSize.x, TilePalletData.TileStride * TilemapSize.y, 8);
-        Render.enableRandomWrite = true;
-        Render.filterMode = FilterMode.Point;
-        Render.anisoLevel = 1;
+        Render = new RenderTexture(TileDimension * TilemapSize.x, WallTilePalletData.TileStride * TilemapSize.y, 8)
+        {
+            enableRandomWrite = true,
+            filterMode = FilterMode.Point,
+            anisoLevel = 1
+        };
         Render.Create();
 
-        FloorRenderTexture = new RenderTexture(TileDimension * TilemapSize.x, TilePalletData.TileStride * TilemapSize.y, 8);
-        FloorRenderTexture.enableRandomWrite = true;
-        FloorRenderTexture.filterMode = FilterMode.Point;
-        FloorRenderTexture.anisoLevel = 1;
+        FloorRenderTexture = new RenderTexture(TileDimension * TilemapSize.x, WallTilePalletData.TileStride * TilemapSize.y, 8)
+        {
+            enableRandomWrite = true,
+            filterMode = FilterMode.Point,
+            anisoLevel = 1
+        };
         FloorRenderTexture.Create();
 
         Rows = new RawImage[TilemapSize.y];
@@ -193,27 +201,28 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
             CRow.sortingOrder = i * 2 + 1;
             RectTransform RT = Row.GetComponent<RectTransform>();
             RT.sizeDelta = Vector2.zero;
-            RT.position = new Vector3(0, -i + 0.75f);
+            float StrideHeight = ((float)WallTilePalletData.TileStride / TileDimension) / TilesPerUnit;
+            RT.position = new Vector3(0, -(i / TilesPerUnit) + StrideHeight / 2);
             Rows[i] = Row.AddComponent<RawImage>();
-            Rows[i].rectTransform.sizeDelta = new Vector2(TilemapSize.x, 1.5f);
+            Rows[i].rectTransform.sizeDelta = new Vector2(TilemapSize.x / TilesPerUnit, StrideHeight);
             Rows[i].material = Resources.Load<Material>("Materials/LitMaterial");
             Rows[i].texture = Render;
             Rows[i].uvRect = new Rect(new Vector2(0, i * 1f / TilemapSize.y), new Vector2(1, 1f / TilemapSize.y));
         }
 
         //Set Render Texture
-        Compute.SetTexture(ComputeKernel, "Result", Render);
+        WallCompute.SetTexture(ComputeKernel, "Result", Render);
         FloorCompute.SetTexture(ComputeKernel, "Result", FloorRenderTexture);
         //Set Tile Dimensions
-        Compute.SetInt("TileDimension", TileDimension);
+        WallCompute.SetInt("TileDimension", TileDimension);
         FloorCompute.SetInt("TileDimension", TileDimension);
         //Set Map Dimensions
-        Compute.SetInt("MapHeight", TilemapSize.y);
+        WallCompute.SetInt("MapHeight", TilemapSize.y);
         FloorCompute.SetInt("MapHeight", TilemapSize.y);
 
         //Set TilePallet
-        Compute.SetTexture(ComputeKernel, "TilePallet", TilePalletData.Pallet);
-        Compute.SetInt("TileStride", TilePalletData.TileStride);
+        WallCompute.SetTexture(ComputeKernel, "TilePallet", WallTilePalletData.Pallet);
+        WallCompute.SetInt("TileStride", WallTilePalletData.TileStride);
         FloorCompute.SetTexture(ComputeKernel, "TilePallet", FloorTilePalletData.Pallet);
         FloorCompute.SetInt("TileStride", FloorTilePalletData.TileStride);
 
@@ -221,33 +230,37 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
         //Map = new Tile[TilemapSize.x * TilemapSize.y];
         Walls = new Tile[4] { new Tile(0, 2, 0, 0), new Tile(0, 2, 1, 1),
                             new Tile(1, 2, 0, 1), new Tile(1, 2, 1, 0) }; //Testing
-        MapBuffer = new ComputeBuffer(Walls.Length, sizeof(int) * 4);
+        WallBuffer = new ComputeBuffer(Walls.Length, sizeof(int) * 4);
         Floor = new Tile[4] { new Tile(0, 2, 0, 1), new Tile(0, 2, 1, 1),
                             new Tile(1, 2, 0, 1), new Tile(1, 2, 1, 1) }; //Testing
         FloorBuffer = new ComputeBuffer(Floor.Length, sizeof(int) * 4);
 
         //Set the buffer
-        Compute.SetBuffer(ComputeKernel, "Map", MapBuffer);
+        WallCompute.SetBuffer(ComputeKernel, "Map", WallBuffer);
         FloorCompute.SetBuffer(ComputeKernel, "Map", FloorBuffer);
     }
 
     private void GenerateDefaultTileData()
     {
-        if (TilePalletData == null)
+        if (WallTilePalletData == null)
         {
-            TilePalletData = new TilePallet();
-            TilePalletData.Pallet = Resources.Load<Texture2D>("TilemapPallets/Default");
-            TilePalletData.NumTiles = 2;
-            TilePalletData.TileStride = 48; //32 + 16
-            TilePalletData.FrameCount = new int[2] { 2, 2 };
+            WallTilePalletData = new TilePallet
+            {
+                Pallet = Resources.Load<Texture2D>("TilemapPallets/Default"),
+                NumTiles = 2,
+                TileStride = 48, //32 + 16
+                FrameCount = new int[2] { 2, 2 }
+            };
         }
         if (FloorTilePalletData == null)
         {
-            FloorTilePalletData = new TilePallet();
-            FloorTilePalletData.Pallet = Resources.Load<Texture2D>("TilemapPallets/Default");
-            FloorTilePalletData.NumTiles = 2;
-            FloorTilePalletData.TileStride = 32;
-            FloorTilePalletData.FrameCount = new int[2] { 2, 2 };
+            FloorTilePalletData = new TilePallet
+            {
+                Pallet = Resources.Load<Texture2D>("TilemapPallets/Default"),
+                NumTiles = 2,
+                TileStride = 32,
+                FrameCount = new int[2] { 2, 2 }
+            };
         }
     }
 
@@ -258,8 +271,8 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
 
     public void UpdateRender()
     {
-        MapBuffer.SetData(Walls);
-        Compute.Dispatch(ComputeKernel, TilemapSize.x, TilemapSize.y, 1);
+        WallBuffer.SetData(Walls);
+        WallCompute.Dispatch(ComputeKernel, TilemapSize.x, TilemapSize.y, 1);
         FloorBuffer.SetData(Floor);
         FloorCompute.Dispatch(ComputeKernel, TilemapSize.x, TilemapSize.y, 1);
     }
@@ -269,12 +282,12 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
         UpdateRender();
         for (int i = 0; i < AppliedRenderers.Count; i++)
         {
-            float YOffset = (Self.transform.position.y + TilemapSize.y / 2f) - AppliedRenderers[i].gameObject.transform.position.y;
-            float XOffset = (Self.transform.position.x + TilemapSize.x / 2f) - AppliedRenderers[i].gameObject.transform.position.x;
+            float YOffset = (Self.transform.position.y + TilemapWorldSize.y / 2f) - AppliedRenderers[i].gameObject.transform.position.y;
+            float XOffset = (Self.transform.position.x + TilemapWorldSize.x / 2f) - AppliedRenderers[i].gameObject.transform.position.x;
             if (YOffset >= 0 && XOffset >= 0 &&
-                YOffset <= TilemapSize.y && XOffset <= TilemapSize.x)
+                YOffset <= TilemapWorldSize.y && XOffset <= TilemapWorldSize.x)
             {
-                AppliedRenderers[i].sortingOrder = Mathf.RoundToInt(YOffset) * 2;
+                AppliedRenderers[i].sortingOrder = Mathf.RoundToInt(YOffset * TilesPerUnit) * 2;
             }
         }
     }
@@ -292,8 +305,8 @@ public class TilemapWrapper : AbstractWorldEntity, IUpdatable
     protected override void _Delete()
     {
         //Release buffers
-        if (MapBuffer != null)
-            MapBuffer.Release();
+        if (WallBuffer != null)
+            WallBuffer.Release();
         //Delete Object
         for (int i = 0; i < Rows.Length; i++)
         {
